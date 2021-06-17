3859aeaede8e410fe3e0eb5098bfd43f4d83088b
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2021-06-17 21:00:11 +0530

    chore: 🐛 upload issue with release script
    - the username with `-u` option didn't seem to work for some weird reason.

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-set -euxo pipefail
+set -e
 
 rm -rf dist build
 python -m pip install -U pip
 python -m pip install -U setuptools wheel twine
 python setup.py sdist bdist_wheel
 twine check dist/*
-python -m twine upload -u __token__ dist/*
+python -m twine upload dist/*
