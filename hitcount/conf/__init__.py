ad5592225d23f9655f670d1b0889636afce20ec2
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2021-07-07 17:09:32 +0530

    chore: Rename deprecated settings to make it more explicilty internal

--- hitcount/conf/__init__.py
+++ hitcount/conf/__init__.py
@@ -10,7 +10,7 @@ class LazySettings(LazyObject):
         self._wrapped = Settings(app_settings, django_settings)
 
 
-DEPRECATED_SETTINGS = {
+_DEPRECATED_DJANGO_SETTINGS = {
     'USE_TZ' if django.VERSION > (4, 0) else None,
     'PASSWORD_RESET_TIMEOUT_DAYS' if django.VERSION > (3, 0) else None,
     'DEFAULT_CONTENT_TYPE' if django.VERSION > (2, 2) else None,
@@ -24,7 +24,7 @@ class Settings:
             setattr(self, attr, getattr(item, attr))
             for item in args
             for attr in dir(item)
-            if attr == attr.upper() and attr.upper() not in DEPRECATED_SETTINGS
+            if attr == attr.upper() and attr.upper() not in _DEPRECATED_DJANGO_SETTINGS
         ]
 
 
